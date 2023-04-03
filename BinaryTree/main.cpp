fb8de8c0a3723d76be73ac37bb2690ea6e90b732
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-04-03 13:24:38 +0300

    method print

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -17,12 +17,56 @@ class Tree
 		{
 			cout << "EDestructor:\t" << this << endl;
 		}
-};
+		friend class Tree;
+	}*Root;
+public:
+	Element* getRoot()
+	{
+		return Root;
+	}
+	Tree() :Root(nullptr)
+	{
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Tree()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr) this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight = nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+
+	/////////////////          Methods           /////////////////////////////
+	void print(Element* Root) const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
 };
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
-
+	int n;
+	cout << "¬ведите размер дерева: "; cin >> n;
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100, tree.getRoot());
+	}
+	tree.print(tree.getRoot());
 }
